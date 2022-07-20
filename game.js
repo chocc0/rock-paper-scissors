c2f98054d026359989e689d7ba414871012c3e54
Mandy <mandysun453@gmail.com>
2022-07-20 17:49:08 -0500

    fixed older coding for playOneRound function

--- game.js
+++ game.js
@@ -32,40 +32,33 @@ function playerChoice() {
 
 function changeVal(value) {
     if (value === "rock") {
-        value = 0
-        return value
-    } else if (value === "paper") {
-        value = 1
-        return value
+        value = 0;
+        return value;
     } else if (value === "scissors") {
-        value = 2
-        return value
+        value = 1;
+        return value;
+    } else if (value === "paper") {
+        value = 2;
+        return value;
     }
 }
 
-/*console.log(computerSelection);
-console.log(playerSelection);*/
-
 //create playOneRound function
-
 function playOneRound(playerSelection, computerSelection) {
     computerSelection = computerChoice();
     playerSelection = playerChoice();
     console.log("User plays " + playerSelection + ". Computer plays " + computerSelection + ".");
     let scorePlayer,
         scoreComputer;
-    switch(changeVal(playerSelection) - changeVal(computerSelection)) {
-        case (0):
-            console.log("Tie.");
-            break;
-        case(-1 || 2):
-            scorePlayer++;
-            console.log("Computer wins.");
-            break;
-        case(-2 || 1):
-            scoreComputer++;
-            console.log("Player wins.");
-            break;
+    let result = (changeVal(playerSelection) - changeVal(computerSelection));
+    if (result === 0) {
+        console.log("Tie.");
+    } else if (result === 2 || result === -1){
+        scorePlayer++;
+        console.log("Player wins.");
+    } else if (result === 1 || result === -2) {
+        scoreComputer++;
+        console.log("Computer wins.");
     }
 }
 
